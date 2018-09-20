27835bae092179f66ceab6a6d479dac18ca865b1
Sanjiv Pradhanang <pradhanang.sanjiv@gmail.com>
2018-09-20 14:30:01 -0500

    Missed aria-labels fix

--- RestaurantReview/js/main.js
+++ RestaurantReview/js/main.js
@@ -144,7 +144,7 @@ createRestaurantHTML = (restaurant) => {
   image.alt = 'Promotional picture for ' + restaurant.name; 
   li.append(image);
 
-  const name = document.createElement('h2');
+  const name = document.createElement('h3');
   name.innerHTML = restaurant.name;
   li.append(name);
 
